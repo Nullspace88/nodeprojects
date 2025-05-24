1561a315749b2015f69b8f64940c7ca50d3d65b0
brendan <brendan.oconnor.913@gmail.com>
2025-05-23 19:54:16 -0500

    added a bunch of features and endpoints for newsletter sign ups and other things

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,10 +1,12 @@
 
 const express = require('express')
 const expressHandlebars = require('express-handlebars').engine
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
+const bodyParser = require('body-parser')
+const path = require('path')
+const multipart = require('multiparty')
 
 const app = express()
-
 
 
 // configure Handlebars view enginer
@@ -13,25 +15,50 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => { res.render('home') })
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', {fortune: fortune.getFortune() })
+app.get('/about', handlers.about)
+
+app.get('/headers', handlers.headers)
+
+app.get('/newsletter-signup', handlers.newsletterSignup)
+
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignUpThankYou)
+
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+app.get('/contest/vacation-photo/', handlers.vacationPhoto)
+
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+	if(err) return res.status(500).send({ error: err.message })
+	handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
 })
 
-app.use((req, res) => {
-    res.status(404)
-    res.send('404 - Not Found')
-})
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.send('500 - Server Error')
-})
 
-app.listen(port, () => console.log(`Express started on http://localhost:${port};`))
+app.use(handlers.notFound)
+
+app.use(handlers.serverError)
+
+if(require.main === module) {
+    app.listen(port, () => {
+	console.log(`Express started on http://localhost:${port};`)
+    })
+} else {
+    module.exports = app
+}
